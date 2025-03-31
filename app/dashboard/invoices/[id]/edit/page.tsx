5fa23b2f57da490c1357124b1562534f55661692
Shayan <nwryshayan500@gmail.com>
2025-03-31 11:29:47 +0330

    Chapter 16: Adding Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,6 +4,10 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 
+export const metadata = {
+  title: 'Edit'
+}
+
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params
   const id = params.id
